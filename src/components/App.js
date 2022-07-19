e32de91ec183bc7cba3fa57ac8a7b91569b4a4ae
Daniel <danielbellinijf21@gmail.com>
2022-07-19 02:36:19 -0300

    troca da primeira tela ao clicar no button

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
+import react from "react";
 import "../assets/styles/reset.css"
 import "../assets/styles/style.css"
+import Flashcards from "./Flashcards";
 import Menu from "./Menu";
 
+
+
 export default function App (){
+
+    const [init, setInit] = react.useState(true);
+
+
     return(
-        <div>
-            <Menu/>
-        </div>
+        <>
+            {init ?
+                <Menu setInit={setInit} />
+                :
+                <Flashcards />
+            }
+        </>
     )
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,11 @@
 import logo from "../assets/images/logo.png"
-export default function Menu (){
+
+export default function Menu ({ setInit }){
     return (
         <div className="menu">
-            <img src ={logo} alt="Logo"/>
+            <img src ={logo} alt="Logo Zap Recall"/>
             <h1>ZapRecall</h1>
-            <button>Iniciar Recall!</button>
+            <button onClick={()=> setInit(false)}>Iniciar Recall!</button>
         </div>
     )
 }
